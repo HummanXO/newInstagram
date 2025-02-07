18ab3fd49d66b44fad53f3e1a2c344115612c62d
HummanXO <rxkolchin@gmail.com>
2025-02-07 09:59:18 +0700

    Initial Commit

---
+++ newInstagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  newInstagram
+//
+//  Created by Alexandr on 2/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
